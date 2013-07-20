c0851627b2e2399182f829eb8955df5592d8240b
Stefan Petrov <stefanvpetrov@gmail.com>
2013-07-20 16:32:54 +0300

    helloworldjscommit

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+/**
+ * Created with PyCharm.
+ * User: stefan
+ * Date: 7/20/13
+ * Time: 4:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+console.log("hello world");
